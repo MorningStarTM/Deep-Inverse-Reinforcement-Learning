feb14a4f7855af0053737f843c9cc68f10ad46be
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-09-08 23:37:16 +0530

    bc created and train func created

--- algorithms/behaviour_cloning.py
+++ algorithms/behaviour_cloning.py
@@ -18,3 +18,45 @@ class NeuralNetwork(nn.Module):
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+
+
+class BehaviorClonning:
+    def __init__(self, input_dim, output_dim, learning_rate=1e-3):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = NeuralNetwork(input_dim, output_dim).to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        self.criterion = nn.CrossEntropyLoss()
+        self.best_accuracy = 0.0
+        self.path = "artifacts\\model.pth"
+
+    
+    def train(self, X_train, y_train, epochs=10, batch_size=32):
+        """
+        Function for training 
+
+        Args: 
+            X_train (tensot) data without lable
+            y_train (tensor) label
+            epochs (int)
+            batch_size (int)
+        """
+        self.model.train()
+        for epoch in range(epochs):
+            permutation = torch.randperm(X_train.size()[0])
+            for i in range(0, X_train.size()[0], batch_size):
+                indices = permutation[i:i + batch_size]
+                batch_X, batch_y = X_train[indices].to(self.device), y_train[indices].to(self.device)
+
+                self.optimizer.zero_grad()
+                outputs = self.model(batch_X)
+                loss = self.criterion(outputs, batch_y)
+                loss.backward()
+                self.optimizer.step()
+
+            #  save the  model when loss is reduce
+            train_accuracy = self.evaluate(X_train, y_train)
+            print(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item()}, Training Accuracy: {train_accuracy:.4f}')
+            if self.best_accuracy < train_accuracy:
+                self.best_accuracy = train_accuracy
+                self.save_model()
